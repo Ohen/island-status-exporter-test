74b64b764d7f15f1d5184f37e68357511e7199aa
ohen <matoda88@naver.com>
2017-07-20 11:41:11 +0900

    refactor: changed to use two collection data

--- src/status-exporter.ts
+++ src/status-exporter.ts
@@ -6,109 +6,116 @@ import fs = require('fs-extra');
 let fileName: string;
 let statusExport: boolean;
 let cacheData: { [type: string]: { [name: string]: { totalTime?: number, count?: number, startAt: number } } } = {};
-let data = {};
+let tmpData = {};
+let calculatedData = {};
+
+async function moveJsonFile(oldPath, newPath): Promise<any> {
+  console.log('move ', oldPath, newPath);
+  return await fs.move(oldPath, newPath, { clobber: true });
+}
+
+function clearData() {
+  cacheData = {};
+  calculatedData = {};
+}
+
+function setDecimalPoint(int: number): number {
+  return Number(int.toFixed(2));
+}
+
+async function lookupProcess(): Promise<any> {
+  const usageOptions = { keepHistory: true };
+  return new Promise((res, rej) => {
+    usage.lookup(process.pid, usageOptions, function (err, result) {
+      if (err) {
+        throw new Error('Failed lookup process information' + err);
+      }
+
+      calculatedData['cpu'] = setDecimalPoint(result.cpu);
+      calculatedData['memory'] = setDecimalPoint((result.memory / 1024 / 1024)); //MB
+      return res();
+    });
+  })
+}
 
 export namespace StatusExporter {
-  export function initialize(using: boolean, option: {name?: string, hostname?: string, servicename?: string}) {
+  export function initialize(using: boolean, option: { name?: string, hostname?: string, servicename?: string }) {
     option = option || {};
     statusExport = using;
-    if(!option.name) option.name = option.hostname + '.' + option.servicename + '.' + 'status.json';
+    if (!option.name) option.name = option.hostname + '.' + option.servicename + '.' + 'status.json';
     console.log('file name', option.name);
-    if(option.name && !option.name.match('.json')) option.name += '.json';
+    if (option.name && !option.name.match('.json')) option.name += '.json';
     fileName = (option.name || 'status.json');
     return fileName;
   }
 
   export async function saveStatusJsonFile() {
-    await lookupProcess();
     await calculateAvgStatus();
-    await fs.writeFileSync(fileName + '.tmp', JSON.stringify(data, null, 2), 'utf8');
-    clearData();
+    await fs.writeFileSync(fileName + '.tmp', JSON.stringify(calculatedData, null, 2), 'utf8');
     return await moveJsonFile(fileName + '.tmp', fileName);
-  }
-
-  async function lookupProcess() {
-    const usageOptions = { keepHistory: true };
-    return new Promise((res, rej) => {
-        usage.lookup(process.pid, usageOptions, function(err, result) {
-          if (err) {
-              console.log("error:" + err + '\n');
-              return;
-          }
-          data['cpu'] = Number((result.cpu).toFixed(2));
-          data['memory'] = Number((result.memory / 1024 / 1024).toFixed(2)); //MB
-          return res();
-      });
-    })
-  }
-
-  async function moveJsonFile(oldPath, newPath) {
-    console.log('move ', oldPath, newPath);
-    return await fs.move(oldPath, newPath, { clobber: true });
-  }
-
-  function clearData() {
-    cacheData = {};
-    data = {};
   }
 
   export async function calculateAvgStatus() {
     if (!statusExport) return;
-    return await _.forEach(cacheData, async (value, type) => {
+    tmpData = _.clone(cacheData);
+    clearData();
+    await lookupProcess();
+    return await _.forEach(tmpData, async (value, type) => {
       await _.forEach(value, (v, k) => {
-        let AvgTime;
-        if(v.totalTime) AvgTime = v.totalTime / v.count;
         const measuringTime = ((+new Date() - v.startAt) / 1000) || 0.001;
-        const TPS = v.count / measuringTime;
-        if(!data[type]) data[type] = {};
+        const TPS = setDecimalPoint(v.count / measuringTime);
+        if (!calculatedData[type]) calculatedData[type] = {};
 
-        data[type][k] = {
+        calculatedData[type][k] = {
           TPS
         };
-        switch (type){
+
+        if (!v.totalTime) return;
+        const avgTime = v.totalTime / v.count;
+        switch (type) {
           case 'event':
-            data[type][k]['AvgReceiveMessageTimeByMQ'] = AvgTime;
+            calculatedData[type][k]['AvgReceiveMessageTimeByMQ'] = setDecimalPoint(avgTime);
             break;
           case 'rpc':
-            data[type][k]['AvgResponseTime'] = AvgTime;
+            calculatedData[type][k]['AvgResponseTime'] = setDecimalPoint(avgTime);
             break;
           default:
-            data[type][k]['AvgTime'] = AvgTime;
+            calculatedData[type][k]['AvgTime'] = setDecimalPoint(avgTime);
         }
       });
     });
   }
 
-  export async function pushTransactionData(type, name) {
+  export async function pushTransactionData(type, key) {
     if (!statusExport) return;
-    if (!cacheData[type] || !cacheData[type][name]) {
+    if (!cacheData[type] || !cacheData[type][key]) {
       cacheData[type] = cacheData[type] || {};
-      if (!cacheData[type][name]) cacheData[type][name] = { count: 1, startAt: +new Date() };
+      if (!cacheData[type][key]) cacheData[type][key] = { count: 1, startAt: +new Date() };
       return;
     }
-    cacheData[type][name]['count'] = cacheData[type][name]['count'] + 1;
+    cacheData[type][key]['count'] = Number(cacheData[type][key]['count'] || 0) + 1;
   };
 
-  export async function pushTimeData(type, name, time){
+  export async function pushTimeData(type, key, time) {
     if (!statusExport) return;
-    if (!cacheData[type] || !cacheData[type][name]) {
+    if (!cacheData[type] || !cacheData[type][key]) {
       cacheData[type] = cacheData[type] || {};
-      if (!cacheData[type][name]) cacheData[type][name] = { totalTime: time, startAt: +new Date() };
+      if (!cacheData[type][key]) cacheData[type][key] = { totalTime: time, startAt: +new Date() };
       return;
     }
 
-    cacheData[type][name]['totalTime'] = Number(cacheData[type][name]['totalTime'] || 0) + time;
+    cacheData[type][key]['totalTime'] = Number(cacheData[type][key]['totalTime'] || 0) + time;
   }
- 
-  export async function pushTransactionAndTimeData(type, name, time){
+
+  export async function pushTransactionAndTimeData(type, key, time) {
     if (!statusExport) return;
-    if (!cacheData[type] || !cacheData[type][name]) {
+    if (!cacheData[type] || !cacheData[type][key]) {
       cacheData[type] = cacheData[type] || {};
-      if (!cacheData[type][name]) cacheData[type][name] = { totalTime: time, count: 1, startAt: +new Date() };
+      if (!cacheData[type][key]) cacheData[type][key] = { totalTime: time, count: 1, startAt: +new Date() };
       return;
     }
 
-    ++cacheData[type][name]['count'];
-    cacheData[type][name]['totalTime'] = Number(cacheData[type][name]['totalTime'] || 0) + time;
+    ++cacheData[type][key]['count'];
+    cacheData[type][key]['totalTime'] = Number(cacheData[type][key]['totalTime'] || 0) + time;
   }
 }
